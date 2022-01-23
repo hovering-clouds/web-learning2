adf02864f2431e88c0938dab0b85b977c87b0f38
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2022-01-23 11:39:39 +0800

    add a new act! When click hc,it will boop at you! also add a text box that display when clicked and can easily be extended.

--- scripts/script-hc.js
+++ scripts/script-hc.js
@@ -1,35 +1,48 @@
 /* define some global variables */
+const bubbleDiv = document.createElement("div");//外部容器(a container for textbox and triangle)
+const bubble = document.createElement("div");//对话气泡框(a text box with a small triangle)
+const tri = document.createElement("div");//气泡框尖角(small triangle append to the text box)
 const canvas = document.createElement("canvas");
 const body = document.querySelector('body');
 const width = canvas.width = 200;
 const height = canvas.height = 200;
 const ctx = canvas.getContext('2d');
 body.appendChild(canvas);
+body.appendChild(bubbleDiv);
+bubbleDiv.appendChild(bubble);
+bubbleDiv.appendChild(tri);
 
 /* set css style */
 /* or you can add an extra css file which manipulate .hclive */
 canvas.setAttribute('class','hclive');
+canvas.addEventListener('click',drawBoop)
 canvas.style.position = 'fixed';
 canvas.style.bottom = '0px';
 canvas.style.right = '0px';
+bubbleDiv.setAttribute('class','container-hc');
+bubble.setAttribute('class','textbox-hc');
+tri.setAttribute('class','triangle-hc');
+bubble.textContent = '你好，游客';
 
 //contex initialization
 ctx.fillStyle = 'rgba(255,255,255,0)';
 ctx.fillRect(0,0,width,height);
 
 //load image
-let image = new Image();
-image.src = 'images/hc-sit-blink2.png';
-image.onload = draw;
+let imageSitBlink = new Image();
+imageSitBlink.src = 'images/hc-sit-blink2.png';
+imageSitBlink.onload = drawBlink;
+let imageSitBoop = new Image();
+imageSitBoop.src = 'images/hc-boop-sit.png';
 
-//draw routine
 let cnt = 0;//to get different frames in cycle
 let velocity = 3;//to control how fast the animation plays
-let blinkFreq = 120;
-const framenum = 6;
-const imgWidth = 156;
-const imgHeight = 168;
-function draw() {
+let framesPerCycle = 120;//to indirectly control the proporion of main act(eg.blink)
+let framenum = 6;
+let imgWidth = 156;
+let imgHeight = 168;
+
+function drawBlink() {
   let ind;
   if (cnt % 3 === 0) {
       let cnt2 = cnt/3;
@@ -39,21 +52,74 @@ function draw() {
       ind = 0;
     }
     ctx.drawImage(
-      image,
+      imageSitBlink,
       imgWidth * ind,
       0,
       imgWidth,
       imgHeight,
-      40,//canvas x-bias
+      width-imgWidth+6,//canvas x-bias
       40,//canvas y-bias
       imgWidth,
       imgHeight
     );
   }
-  if (cnt === blinkFreq) {
-    cnt = 0;
+  if (cnt === framesPerCycle) {
+      cnt = 0;
   } else {
     cnt++;
   }
-  window.requestAnimationFrame(draw);
+  raf1 = window.requestAnimationFrame(drawBlink);
+}
+function initImage1(){
+cnt = 0;//to get different frames in cycle
+velocity = 3;//to control how fast the animation plays
+framesPerCycle = 120;//to indirectly control the proporion of main act(eg.blink)
+framenum = 6;
+imgWidth = 156;
+imgHeight = 168;
+}
+function initImage2(){
+  cnt = 0;//to get different frames in cycle
+  velocity = 3;//to control how fast the animation plays
+  framesPerCycle = 120;
+  framenum = 17;
+  imgWidth = 159;
+  imgHeight = 174;
+}
+function drawBoopOnce(){
+  let ind;
+  if (cnt % 3 === 0) {
+      let cnt2 = cnt/3;
+    if (cnt2 < framenum) {
+      ind = cnt2;
+    } else {
+      ind = 0;
+    }
+    ctx.clearRect(0,0,width,height);
+    ctx.drawImage(
+      imageSitBoop,
+      imgWidth * ind,
+      0,
+      imgWidth,
+      imgHeight,
+      width-imgWidth+6,//canvas x-bias
+      34,//canvas y-bias(for alignment, this should be img1.ybias-img1.width+this.width)
+      imgWidth,
+      imgHeight
+    );
+  }
+  if (cnt === framesPerCycle) {
+    initImage1();
+    bubbleDiv.style.opacity = 0;
+    raf1 = window.requestAnimationFrame(drawBlink);
+  } else {
+    cnt++;
+    raf1 = window.requestAnimationFrame(drawBoopOnce);
+  }
+}
+function drawBoop(){
+    window.cancelAnimationFrame(raf1);
+    bubbleDiv.style.opacity = 0.9;
+    initImage2();
+    drawBoopOnce();
 }
